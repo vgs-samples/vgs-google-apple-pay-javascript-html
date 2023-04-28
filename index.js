57e730a1129b70be99076868ce463d03e8f268af
FionnT <fionntolster@gmail.com>
2023-04-28 21:27:23 +0800

    feat: heroku

--- index.js
+++ index.js
@@ -11,23 +11,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.static('static'))
-app.set('views', path.join(__dirname, 'views'));
 
-// Create a NodeJS HTTPS listener on port 3000 that points to the Express app
-// Use a callback function to tell when the server is created.
-https
-  .createServer(
-    app
-  )
-  .listen(process.env.PORT || 5000, () => {
-    console.log("Server is runing at port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is runing at port " + process.env.PORT);
   });
 
 // Create an try point route for the Express app listening on port 4000.
 // This code tells the service to listed to any request coming to the / route.
 // Once the request is received, it will display a message "Hello from express server."
 app.get('/', (req,res)=>{
-    res.sendFile("./static/index.html");
+    res.sendFile("index.html")
 })
 
 app.get('/session', (req, res) => {
